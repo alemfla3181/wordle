24239ca9cd1e2f7d35fb72603ddb8150fa65c391
alemfla3181 <alemfla3181@naver.com>
2022-02-25 17:00:41 +0900

    test1

--- src/components/views/LandingPage/LandingPage.js
+++ src/components/views/LandingPage/LandingPage.js
@@ -8,21 +8,17 @@ function LandingPage() {
   const handleChange = e => {
     const { maxLength, value } = e.target;
     if (value.length >= maxLength) {
-      const nextSibling = document.querySelector(
-        "input");
-      console.log(document.querySelector(`input[name=n-${1}]`));
-      if (nextSibling !== null)
-        nextSibling.focus();
+      e.target.nextSibling.focus();
     }
   }
 
   return (
     <div>
-      <input name='n-1' maxLength={1} onChange={handleChange}></input>
-      <input name='n-2' maxLength={1} onChange={handleChange}></input>
-      <input name='n-3' maxLength={1} onChange={handleChange}></input>
-      <input name='n-4' maxLength={1} onChange={handleChange}></input>
-      <input name='n-5' maxLength={1} onChange={handleChange}></input>
+      <input maxLength={1} onChange={handleChange}></input>
+      <input maxLength={1} onChange={handleChange}></input>
+      <input maxLength={1} onChange={handleChange}></input>
+      <input maxLength={1} onChange={handleChange}></input>
+      <input maxLength={1} onChange={handleChange}></input>
 
     </div>
   )
