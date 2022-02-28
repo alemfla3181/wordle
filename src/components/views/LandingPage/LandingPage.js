af652e5a5daf74294dd84253e349f72cb93ebfc6
alemfla3181 <alemfla3181@naver.com>
2022-02-28 16:08:03 +0900

    button

--- src/components/views/LandingPage/LandingPage.js
+++ src/components/views/LandingPage/LandingPage.js
@@ -4,45 +4,57 @@ import WordChecking from '../WordChecking/WordChecking';
 
 function LandingPage() {
   const [Value, setValue] = useState("");
+  const [Show1, setShow1] = useState(false);
+  const [Show2, setShow2] = useState(false);
+  const [Show3, setShow3] = useState(false);
+  const [Show4, setShow4] = useState(false);
 
-  const handleChange = e => {
-    const { maxLength, value } = e.target;
-    if (value.length >= maxLength) {
-      e.target.nextSibling.focus();
-    }
-    setValue({ ...Value, [e.target.name]: value});
-  }
 
-  const checkKeyPress = e => {
-    if (e.keyCode === 8 && e.target.value.length <= 0) {
-      e.target.previousSibling.focus();
-    } else if (e.key === 'Enter') {
-      clickResult();
-    }
-  }
 
-  const clickResult = () => {
-    // console.log(Value[5]);
-    for (let i = 1; i <= 5; i++) {
-      if (Value[i] === undefined || Value[i] === '') {
-        alert('Full all!!!!');
-        return 0;
-      }
-    }
-    // console.log(Value);
-    <WordChecking />
+  // const handleChange = e => {
+  //   const { maxLength, value } = e.target;
+  //   if (value.length >= maxLength) {
+  //     e.target.nextSibling.focus();
+  //   }
+  //   setValue({ ...Value, [e.target.name]: value });
+  // }
+
+  // const checkKeyPress = e => {
+  //   if (e.keyCode === 8 && e.target.value.length <= 0) {
+  //     e.target.previousSibling.focus();
+  //   } else if (e.key === 'Enter') {
+  //     clickResult();
+  //   }
+  // }
+
+  // const clickResult = () => {
+  //   console.log(Value);
+  //   for (let i = 1; i <= 5; i++) {
+  //     if (Value[i] === undefined || Value[i] === '') {
+  //       alert('Full all!!!!');
+  //       return 0;
+  //     }
+  //   }
+  //   // console.log(Value);
+  //   setShow1(true);
+  //   return console.log(Value)
+  // }
+
+  const frontValue = e => {
+    console.log(e);
+    (Show1 ? (Show2 ? (Show3 ? (Show4 ? alert("WOW!") : setShow4(true)) : setShow3(true)) : setShow2(true)) : setShow1(true));
   }
 
   return (
     <div className='app'>
-      <input name='1' maxLength={1} onChange={handleChange} onKeyDown={checkKeyPress}></input>
-      <input name='2' maxLength={1} onChange={handleChange} onKeyDown={checkKeyPress}></input>
-      <input name='3' maxLength={1} onChange={handleChange} onKeyDown={checkKeyPress}></input>
-      <input name='4' maxLength={1} onChange={handleChange} onKeyDown={checkKeyPress}></input>
-      <input name='5' maxLength={1} onChange={handleChange} onKeyDown={checkKeyPress}></input>
+      <WordChecking propValue={frontValue} />
+      {Show1 && <WordChecking propValue={frontValue}  />}
+      {Show2 && <WordChecking propValue={frontValue}  />}
+      {Show3 && <WordChecking propValue={frontValue}  />}
+      {Show4 && <WordChecking propValue={frontValue}  />}
 
       <div>
-        <button onClick={clickResult}>결과</button>
+        <button onClick={frontValue}>결과</button>
       </div>
 
     </div>
--- src/components/views/WordChecking/WordChecking.js
+++ src/components/views/WordChecking/WordChecking.js
@@ -1,15 +1,43 @@
 import React, { useState } from 'react'
 
 function WordChecking(props) {
-  console.log(props.Value);
+  const [Value, setValue] = useState("");
+
+  const handleChange = e => {
+    const { maxLength, value } = e.target;
+    if (value.length >= maxLength && e.target.nextSibling !== null) {
+      e.target.nextSibling.focus();
+    }
+    setValue({ ...Value, [e.target.name]: value });
+  }
+
+  const checkKeyPress = e => {
+    if (e.keyCode === 8 && e.target.value.length <= 0 && e.target.previousSibling !== null) {
+      e.target.previousSibling.focus();
+    } else if (e.key === 'Enter') {
+      clickResult();
+    }
+  }
+
+  const clickResult = () => {
+    // console.log(Value);
+    for (let i = 1; i <= 5; i++) {
+      if (Value[i] === undefined || Value[i] === '') {
+        alert('Full all!!!!');
+        return 0;
+      }
+    }
+    // console.log(Value);
+    return props.propValue(Value);
+  }
 
   return (
     <div>
-      <input></input>
-      <input></input>
-      <input></input>
-      <input></input>
-      <input></input>
+      <input name='1' maxLength={1} onChange={handleChange} onKeyDown={checkKeyPress}></input>
+      <input name='2' maxLength={1} onChange={handleChange} onKeyDown={checkKeyPress}></input>
+      <input name='3' maxLength={1} onChange={handleChange} onKeyDown={checkKeyPress}></input>
+      <input name='4' maxLength={1} onChange={handleChange} onKeyDown={checkKeyPress}></input>
+      <input name='5' maxLength={1} onChange={handleChange} onKeyDown={checkKeyPress}></input>
     </div>
   )
 }
