c8efa90cb85cf4f31c8d1fd6eaa38175f6a998c2
alemfla3181 <alemfla3181@naver.com>
2022-04-14 19:41:29 +0900

    moment 경고 수정

--- client/src/components/views/GetTime/GetTime.js
+++ client/src/components/views/GetTime/GetTime.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import moment from 'moment';
 
 function GetTime(props) {
-  const resetTime = JSON.parse(window.localStorage.getItem("Time")) || moment().endOf('day').format("YYYMMDD HH:mm:ss");
+  const resetTime = JSON.parse(window.localStorage.getItem("Time")) || moment().endOf('day').format("YYYY-MM-DD HH:mm:ss");
   const [OnReset, setOnReset] = useState(false);
 
   useEffect(() => {
@@ -15,7 +15,7 @@ function GetTime(props) {
       setOnReset(true);
     } else {
       if (window.localStorage.getItem("Time")) {
-        console.log(moment().format("YYYMMDD HH:mm:ss"));
+        // console.log(moment().format("YYYMMDD HH:mm:ss"));
         console.log("이미있음");
       } else {
         window.localStorage.setItem("Time", JSON.stringify(resetTime));
