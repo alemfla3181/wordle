2dff197cff940ea600c7c64c80c8a3e816f06a4d
alemfla3181 <alemfla3181@naver.com>
2022-04-19 23:41:58 +0900

    option

--- client/src/components/source/Source.js
+++ client/src/components/source/Source.js
@@ -1,5 +1,32 @@
+import moment from "moment";
+
 export function f1() {
-  const answerResult = ['', 'h', 'e', 'l', 'l', 'o'];
+  const day = moment().day();
+  let answerResult;
+  switch (day) {
+    case 0:
+      answerResult = ['', 'h', 'e', 'l', 'l', 'o'];
+      break;
+    case 1:
+      answerResult = ['', 'p', 'r', 'o', 'x', 'y'];
+      break;
+    case 2:
+      answerResult = ['', 'n', 'a', 'v', 'a', 'l'];
+      break;
+    case 3:
+      answerResult = ['', 'a', 'b', 'a', 's', 'e'];
+      break;
+    case 4:
+      answerResult = ['', 's', 't', 'a', 'f', 'f'];
+      break;
+    case 5:
+      answerResult = ['', 't', 'r', 'a', 'c', 'k'];
+      break;
+    case 6:
+      answerResult = ['', 'b', 'r', 'a', 'v', 'o'];
+      break;
+  }
+  
 
   return answerResult;
 }
